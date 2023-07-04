57b44e4b8321624b7c71381cee16ab52aeee27a8
newsty <xadichaermatova19@gmail.com>
2023-07-04 23:38:54 +0500

    class js

---
+++ main.js
@@ -0,0 +1,110 @@
+// 1-vazifa
+
+// class book{
+//     constructor(name,juz){
+//         this.name=name;
+//         this.juz=juz;
+//     }
+// }
+// const kitob=new book("Shaytonvachchaning nayranglari",6);
+
+// class phone{
+//     constructor(name,soum){
+//         this.name=name;
+//         this.soum=soum;
+//     }
+// }
+// const Readme=new phone("Redame","1 200 000 so'm");
+
+
+// class noutbuk{
+//     constructor(name,color,soum){
+//         this.name=name;
+//         this.color=color;
+//         this.soum=soum;
+//     }
+// }
+// const aser=new noutbuk("Aser","bleak","9 000 000 so'm")
+
+// class watch{
+//     constructor(name,soum,color){
+//         this.name=name;
+//         this.soum=soum;
+//         this.color=color;
+//     }
+// }
+// const Hoco=new watch("Haco","450 000 so'm","blue")
+
+// class TV{
+//     constructor(name,soum){
+//         this.name=name;
+//         this.soum=soum;
+//     }
+// }
+// const Artel= new TV("Artel"," 5 000 000 so'm");
+// const samsung= new TouchEvent("samsung","7 500 000 so'm");
+
+
+
+
+// 2-vazifa
+
+// class Keyboard{
+//     constructor(name,type){
+//         this.name=name;
+//         this.tupe=type;
+//     }
+//   Alt(){
+//     console.log("Alt+Tab bosilganda boshqa oynani ochib beradi");
+//   }
+//   Ctrl(){
+//     console.log("Ctrl + C nusxa oladi");
+//   }
+//   Shift(){
+//     console.log("Shift + harifni bosilganda harifni kotta qilib beradi ");
+//   }
+//   Delete(){
+//     console.log("O'chirb beradi");
+//   }
+//   CapsLock(){
+//     console.log("Brcha harflarni kotta qilib beradi");
+//   }
+//   Enter(){
+//     console.log("Enter pasgi qatorga tushurib beradi");
+//   }
+//   PrtScSysRq(){
+//     console.log("Rasimga oladi");
+//   }
+// }
+// const key=new Keyboard("klaviatura","imkoniyatlari");
+
+
+// 3-vazifa
+
+// function Person(name,years,gender){
+//     this.name=name;
+//     this.years=years;
+//     this.gender=gender;
+// }
+// const Malika=new Person("Malika",1996,"girl");
+
+// function Kasha(name,number,soum){
+//     this.name=name;
+//     this.number=number;
+//     this.soum=soum;
+// }
+// const Nan=new Kasha("NAN",3,"97 000 so'm")
+
+// function Naushnik(name,color,soum){
+//     this.name=name;
+//     this.color=color;
+//     this.soum=soum;
+// }
+// const Huawei=new Naushnik("HUAWEI","black","450 000 so'm");
+
+// function ElektrChoynak(name,color,soum){
+//     this.name=name;
+//     this.color=color;
+//     this.soum=soum;
+// }
+// const Shivaki=new ElektrChoynak("Shivaki","Oq","205 000 so'm ")
